091de3176cd35b5e65f94d9ae76f2cdfbdb94e66
PatelManav <58802170+PatelManav@users.noreply.github.com>
2020-12-01 22:35:49 +0530

    original file

--- code/FAT32.c
+++ code/FAT32.c
@@ -73,6 +73,37 @@ int LBAToOffset(int32_t sector)
     return ((sector - 2) * BPB_BytesPerSec) + (BPB_BytesPerSec * BPB_RsvdSecCnt) + (BPB_NumFATs * BPB_FATSz32 * BPB_BytesPerSec);
 }
 
+void getInput()
+{
+    printf("CMD> ");
+
+    memset(cmd_str, '\0', MAX_COMMAND_SIZE);
+
+    while (!fgets(cmd_str, MAX_COMMAND_SIZE, stdin));
+
+    int token_count = 0;
+
+    char *arg_ptr;
+
+    char *working_str = strdup(cmd_str);
+
+    char *working_root = working_str;
+
+    memset(&token, '\0', MAX_NUM_ARGUMENTS);
+
+    memset(&token, '\0', sizeof(MAX_NUM_ARGUMENTS));
+    while (((arg_ptr = strsep(&working_str, WHITESPACE)) != NULL) && (token_count < MAX_NUM_ARGUMENTS))
+    {
+        token[token_count] = strndup(arg_ptr, MAX_COMMAND_SIZE); // v[i] = string
+        if (strlen(token[token_count]) == 0)  // size = 0 is not valid
+        {
+            token[token_count] = NULL;
+        }
+        token_count++;
+    }
+    free(working_root);
+}
+
 void getInput()    // patigyu
 {
     printf("CMD> ");
@@ -157,45 +188,40 @@ void execute()
     {
         printDirectory();
     }
+    else if (strcmp(token[0], "cd") == 0)
+    {
+        if (token[1] == NULL)
+        {
+            printf("ERR: Please provide which directory you would like to open\n");
+            return;
+        }
+        changeDirectory(getCluster(token[1]));
+    }
+    else if (strcmp(token[0], "get") == 0)
+    {
+        get(token[1]);
+    }
+    else if (strcmp(token[0], "stat") == 0)
+    {
+        stat(token[1]);
+    }
     else if (strcmp(token[0], "volume") == 0)
     {
         volume();
+    }
+    else if (strcmp(token[0], "read") == 0)
+    {
+        if (token[1] == NULL || token[2] == NULL || token[3] == NULL)
+        {
+            printf("Please input valid arguments.\n");
+            return;
+        }
+        readFile(token[1], atoi(token[2]), atoi(token[3]));
     }
     else if (strcmp(token[0], "close") == 0)
     {
         closeImage();
     }
-}
-
-void getInput()
-{
-    printf("CMD> ");
-
-    memset(cmd_str, '\0', MAX_COMMAND_SIZE);
-
-    while (!fgets(cmd_str, MAX_COMMAND_SIZE, stdin));
-
-    int token_count = 0;
-
-    char *arg_ptr;
-
-    char *working_str = strdup(cmd_str);
-
-    char *working_root = working_str;
-
-    memset(&token, '\0', MAX_NUM_ARGUMENTS);
-
-    memset(&token, '\0', sizeof(MAX_NUM_ARGUMENTS));
-    while (((arg_ptr = strsep(&working_str, WHITESPACE)) != NULL) && (token_count < MAX_NUM_ARGUMENTS))
-    {
-        token[token_count] = strndup(arg_ptr, MAX_COMMAND_SIZE); // v[i] = string
-        if (strlen(token[token_count]) == 0)  // size = 0 is not valid
-        {
-            token[token_count] = NULL;
-        }
-        token_count++;
-    }
-    free(working_root);
 }
 
 void openImage(char file[])
@@ -239,7 +265,140 @@ void volume()
     printf("Volume name: %s\n", BPB_Volume);
 }
 
-void getDirectoryInfo()
+
+
+void get(char *dirname)   // ambiguous
+{
+    char *dirstring = (char *)malloc(strlen(dirname));
+    strncpy(dirstring, dirname, strlen(dirname));
+    int cluster = getCluster(dirstring);
+    int size = getSizeOfCluster(cluster);
+    FILE *newfp = fopen(token[1], "w");
+    fseek(fp, LBAToOffset(cluster), SEEK_SET);
+    unsigned char *ptr = malloc(size);
+    fread(ptr, size, 1, fp);
+    fwrite(ptr, size, 1, newfp);
+    fclose(newfp);
+}
+
+void formatDirectory(char *dirname)  // converts to capital letters  microsoft - 8.3 filename syster where 8 is filename and 3 is for extension
+{
+    char expanded_name[12];
+    memset(expanded_name, ' ', 12);   // initiliaze with whitespace
+
+    char *token = strtok(dirname, "."); // separated by delimeter "." and stored in token array 
+
+    if (token)
+    {
+        strncpy(expanded_name, token, strlen(token));  // copies token to expanded_name 
+
+        token = strtok(NULL, ".");
+
+        if (token)
+        {
+            strncpy((char *)(expanded_name + 8), token, strlen(token));  // take only first 8 characters of expanded_name
+        }
+
+        expanded_name[11] = '\0';
+
+        int i;
+        for (i = 0; i < 11; i++)
+        {
+            expanded_name[i] = toupper(expanded_name[i]);  // converting everything to uppercase
+        }
+    }
+    else
+    {
+        strncpy(expanded_name, dirname, strlen(dirname));
+        expanded_name[11] = '\0';
+    }
+    strncpy(formattedDirectory, expanded_name, 12);    // ALL capital letters with 8 filename and 3 for extension length
+}
+
+int32_t getCluster(char *dirname)
+{
+    formatDirectory(dirname);  // converts all letter to capital
+    int i;
+    for (i = 0; i < 16; i++)
+    {
+        char *directory = malloc(11);   // allocate 11 bytes
+        memset(directory, '\0', 11);    // set all to '\0'
+        memcpy(directory, dir[i].DIR_Name, 11);   // copies dir name to variable
+
+        if (strncmp(directory, formattedDirectory, 11) == 0)   // compares original name and given name
+        {
+            int cluster = dir[i].DIR_FirstClusterLow;     // initial pointer of that dir
+            return cluster;
+        }
+    }
+    return -1;  // if no such file is present
+}
+
+int32_t getSizeOfCluster(int32_t cluster)   // returns size of provided cluster
+{
+    int i;
+    for (i = 0; i < 16; i++)    // traversing all dir
+    {
+        if (cluster == dir[i].DIR_FirstClusterLow)  // finds correct cluster
+        {
+            int size = dir[i].DIR_FileSize;   // gets size of that dir
+            return size;
+        }
+    }
+    return -1;
+}
+
+void stat(char *dirname)
+{
+    int cluster = getCluster(dirname);     // obtains cluster
+    int size = getSizeOfCluster(cluster);  // gets size of cluster
+    printf("Size: %d\n", size);            // print file size
+    int i;
+    for (i = 0; i < 16; i++)
+    {
+        if (cluster == dir[i].DIR_FirstClusterLow)    // finds its cluster
+        {
+            printf("Attr: %d\n", dir[i].DIR_Attr);
+            printf("Starting Cluster: %d\n", cluster);
+            printf("Cluster High: %d\n", dir[i].DIR_FirstClusterHigh);
+        }
+    }
+}
+
+void changeDirectory(int32_t cluster)    // "cd" implemented
+{
+    if (strcmp(token[1], "..") == 0)  // if it command is "cd .."
+    {
+        int i;
+        for (i = 0; i < 16; i++)
+        {
+            if (strncmp(dir[i].DIR_Name, "..", 2) == 0)  // finds cluster for ..
+            {
+                int offset = LBAToOffset(dir[i].DIR_FirstClusterLow);    // gets offset
+                currentDirectory = dir[i].DIR_FirstClusterLow;           // stores in current dir
+                fseek(fp, offset, SEEK_SET);                             // moves pointer to offset
+                fread(&dir[0], 32, 16, fp);                              // read that content
+                return;
+            }
+        }
+    }
+    int offset = LBAToOffset(cluster);    // gets offset
+    currentDirectory = cluster;           // stores in current dir
+    fseek(fp, offset, SEEK_SET);          // moves pointer to offset
+    fread(&dir[0], 32, 16, fp);           // read that content
+}
+
+void readFile(char *dirname, int position, int numOfBytes)  // reads file starting from the given position upto the number of bytes mentioned
+{
+    int cluster = getCluster(dirname);        // gets value of cluster
+    int offset = LBAToOffset(cluster);        // gets offset
+    fseek(fp, offset + position, SEEK_SET);   // moves pointer from where we need to read the file
+    char *bytes = malloc(numOfBytes);         // allocate given size
+    fread(bytes, numOfBytes, 1, fp);          // reads the entire content till size numofbytes
+    printf("%s\n", bytes);
+}
+
+void getDirectoryInfo()    // prints information
 {
     int i;
     for (i = 0; i < 16; i++)
@@ -248,40 +407,66 @@ void getDirectoryInfo()
     }
 }
 
-void printDirectory()
+void printDirectory()  // works as "ls" command
 {
-    if (fp == NULL)
+    if (fp == NULL) // image file not opened
     {
         printf("No image is opened\n");
         return;
     }
 
-    int offset = LBAToOffset(currentDirectory);
-    fseek(fp, offset, SEEK_SET);
+    int offset = LBAToOffset(currentDirectory);  // get offset for current dir
+    fseek(fp, offset, SEEK_SET);  // moves pointer to offset
 
     int i;
-    for (i = 0; i < 16; i++)
+    for (i = 0; i < 16; i++)  // traversing through all directories
     {
-        fread(&dir[i], 32, 1, fp);
+        fread(&dir[i], 32, 1, fp);  // reading ith directory
 
         if ((dir[i].DIR_Name[0] != (char)0xe5) && (dir[i].DIR_Attr == 0x1 || dir[i].DIR_Attr == 0x10 || dir[i].DIR_Attr == 0x20))
         {
             char *directory = malloc(11);
-            memset(directory, '\0', 11);
-            memcpy(directory, dir[i].DIR_Name, 11);
-            printf("%s\n", directory);
+            memset(directory, '\0', 11);   // initialize directory to '\0'
+            memcpy(directory, dir[i].DIR_Name, 11); // copies directory name
+            printf("%s\n", directory); // print
         }
     }
 }
 
-void closeImage()
+void decToHex(int dec)
 {
-    if (fp == NULL)
+    char hex[100];
+    int i = 1;
+    int j;
+    int temp;
+    while (dec != 0)
+    {
+        temp = dec % 16;
+        if (temp < 10)
+        {
+            temp += 48;
+        }
+        else
+        {
+            temp += 55;
+        }
+        hex[i++] = temp;
+        dec /= 16;
+    }
+    for (j = i - 1; j > 0; j--)
+    {
+        printf("%c", hex[j]);
+    }
+}
+
+void closeImage()  // closes the currently opened image file
+{
+    if (fp == NULL)   // if file is not opened
     {
         printf("Error: File system not open.");
         return;
     }
 
-    fclose(fp);
-    fp = NULL;
+    fclose(fp);   // closes the file
+    fp = NULL;    // sets file pointer to NULL
 }
